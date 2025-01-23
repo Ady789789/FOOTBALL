38e5475a21cea0168fb8a560f3c54a85b643c329
adina778899 <adinamemetea@yahoo.com>
2025-01-22 21:21:31 -0600

    Initial Commit

---
+++ FOOTBALL/FOOTBALLApp.swift
@@ -0,0 +1,17 @@
+//
+//  FOOTBALLApp.swift
+//  FOOTBALL
+//
+//  Created by natural health on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FOOTBALLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
